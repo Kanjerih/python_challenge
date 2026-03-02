03fa59da465a65afd7cbeb0526f9bb347e5c97d3
Ndung'u <phitahn@gmail.com>
2026-03-02 23:40:04 +0300

    Add challenge_1 file

---
+++ challenge_1.py
@@ -0,0 +1,15 @@
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing != "yes":
+    quit()
+
+    print("Okay! let's play :)")
+
+    answer = input("what does cpu stand for? ")
+
+    if answer == "central processing unit":
+
+        print('correct!')
+        
