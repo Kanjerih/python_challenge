4193fd1bd08c45cd64d3209a71225a301e12eb9d
Ndung'u <phitahn@gmail.com>
2026-03-03 00:02:38 +0300

    Add challenge_1 file

--- challenge_1.py
+++ challenge_1.py
@@ -2,14 +2,39 @@ print("Welcome to my computer quiz!")
 
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
-    print("Okay! let's play :)")
+print("Okay! let's play :)")
+score = 0
 
-    answer = input("what does cpu stand for? ")
+answer = input("what does gpu stand for? ")
 
-    if answer == "central processing unit":
+if answer.lower() == "graphic processing unit":
 
-        print('correct!')
-        
+    print('correct!')
+    score += 1
+else:
+    print("Incorrect")
+
+answer = input("what does ram stand for? ")
+
+if answer.lower() == "random access memory":
+
+    print('correct!')
+    score += 1
+else:
+    print("Incorrect")
+
+
+    answer= input("what does psu stand for? ")
+
+if answer.lower() == "power supply":
+
+    print('correct!')
+    score += 1
+else:
+    print("Incorrect")
+
+print("you got " + str(score) + " questions correct!")
+print("you got " + str((score/4)*100) +  "%.")
